a9e645f0f4575bde71e10e30d7247718d554a952
xiezg <xiezg@317hu.com>
2017-05-24 13:02:32 +0800

    网站logo修改

--- imanager-web/src/main/java/com/bozhong/imanager/restful/InsistRest.java
+++ imanager-web/src/main/java/com/bozhong/imanager/restful/InsistRest.java
@@ -47,47 +47,56 @@ public class InsistRest {
         String serviceName = (String) EWebServletContext.getEWebContext().get("serviceName");
         String group = (String) EWebServletContext.getEWebContext().get("group");
         List<ServiceMeta> serviceMetaList = new ArrayList<>();
-        if (StringUtil.isBlank(serviceName) && StringUtil.isBlank(group)) {
-            //分组
-            List<String> groupPaths = getChildrenPath(InsistUtil.getProviderZkPath());
-            if (CollectionUtils.isEmpty(groupPaths)) {
-                return ResultMessageBuilder.build(false, ImanagerErrorEnum.E10002.getError(),
-                        ImanagerErrorEnum.E10002.getMsg()).
-                        toJSONString();
+        //分组
+        List<String> groupPaths = getChildrenPath(InsistUtil.getProviderZkPath());
+        if (CollectionUtils.isEmpty(groupPaths)) {
+            return ResultMessageBuilder.build(false, ImanagerErrorEnum.E10002.getError(),
+                    ImanagerErrorEnum.E10002.getMsg()).
+                    toJSONString();
+        }
+
+        //查询条件分组不为空时
+        if (StringUtil.isNotBlank(group)) {
+            groupPaths = new ArrayList<>();
+            groupPaths.add(group);
+        }
+
+        for (String groupPath : groupPaths) {
+            //服务
+            List<String> serviceGroupPaths = getChildrenPath(ImanagerUtil.getProviderGroupPath(groupPath));
+            if (CollectionUtils.isEmpty(serviceGroupPaths)) {
+                continue;
             }
 
-            for (String groupPath : groupPaths) {
-                //服务
-                List<String> serviceGroupPaths = getChildrenPath(ImanagerUtil.getProviderGroupPath(groupPath));
-                if (CollectionUtils.isEmpty(serviceGroupPaths)) {
+            //查询条件服务名不为空时
+            if (StringUtil.isNotBlank(serviceName)) {
+                serviceGroupPaths = new ArrayList<>();
+                serviceGroupPaths.add(serviceName);
+            }
+
+            for (String serviceGroupPath : serviceGroupPaths) {
+                //版本
+                List<String> versionServiceGroupPaths = getChildrenPath(ImanagerUtil.
+                        getProviderGroupServiceNamePath(groupPath, serviceGroupPath));
+                if (CollectionUtils.isEmpty(versionServiceGroupPaths)) {
                     continue;
                 }
 
-                for (String serviceGroupPath : serviceGroupPaths) {
-                    //版本
-                    List<String> versionServiceGroupPaths = getChildrenPath(ImanagerUtil.
-                            getProviderGroupServiceNamePath(groupPath, serviceGroupPath));
-                    if (CollectionUtils.isEmpty(versionServiceGroupPaths)) {
+                for (String versionServiceGroupPath : versionServiceGroupPaths) {
+                    //IP端口号
+                    List<String> versionServiceGroupPathAndIpPorts = getChildrenPath(ImanagerUtil.
+                            getServiceNameGroupVersionZkPath(groupPath, serviceGroupPath, versionServiceGroupPath));
+                    if (CollectionUtils.isEmpty(versionServiceGroupPathAndIpPorts)) {
                         continue;
                     }
 
-                    for (String versionServiceGroupPath : versionServiceGroupPaths) {
-                        //IP端口号
-                        List<String> versionServiceGroupPathAndIpPorts = getChildrenPath(ImanagerUtil.
-                                getServiceNameGroupVersionZkPath(groupPath, serviceGroupPath, versionServiceGroupPath));
-                        if (CollectionUtils.isEmpty(versionServiceGroupPathAndIpPorts)) {
-                            continue;
-                        }
-
-                        //节点数据
-                        for (String versionServiceGroupPathAndIpPort :
-                                versionServiceGroupPathAndIpPorts) {
-                            String serviceMetaStr = getPathData(ImanagerUtil.getIpPortPath(groupPath, serviceGroupPath,
-                                    versionServiceGroupPath, versionServiceGroupPathAndIpPort));
-                            if (StringUtil.isNotBlank(serviceMetaStr)) {
-                                serviceMetaList.add(InsistUtil.jsonToServiceMeta(serviceMetaStr));
-                            }
-
+                    //节点数据
+                    for (String versionServiceGroupPathAndIpPort :
+                            versionServiceGroupPathAndIpPorts) {
+                        String serviceMetaStr = getPathData(ImanagerUtil.getIpPortPath(groupPath, serviceGroupPath,
+                                versionServiceGroupPath, versionServiceGroupPathAndIpPort));
+                        if (StringUtil.isNotBlank(serviceMetaStr)) {
+                            serviceMetaList.add(InsistUtil.jsonToServiceMeta(serviceMetaStr));
                         }
 
                     }
